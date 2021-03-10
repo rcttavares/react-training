50c0f7b70e9000a2cecccdd34945a362025a1725
Rafael Tavares <rtavares@ciandt.com>
2021-03-10 10:34:15 -0300

    [BEESPI-5072] - Unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import DogDetails from './components/dogDetails/DogDetails';
+import CreateBeerForm from './components/createBeerForm/CreateBeerForm';
+import CreateBeerFormikForm from './components/createBeerFormikForm/CreateBeerFormikForm';
+import { Grid } from '@material-ui/core';
 
 describe('App', () => {
   it('should render correctly', () => {
@@ -8,6 +12,22 @@ describe('App', () => {
       <App />
     );
 
-    expect(wrapper).toBeTruthy();
+    expect(
+      wrapper.matchesElement(
+        <div>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <DogDetails />
+            </Grid>
+            <Grid item xs={6}>
+              <CreateBeerForm />
+            </Grid>
+            <Grid item xs={6}>
+              <CreateBeerFormikForm />
+            </Grid>
+          </Grid>
+        </div>
+      )
+    ).toBe(true);
   });
 });
